3f261de6714cc01f39222be4f623dd1f1b81ad1c
EhsanTgv <EhsanTgv@gmail.com>
2023-02-23 14:02:59 +0300

    Initial Commit

---
+++ NavDemo/NavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavDemoApp.swift
+//  NavDemo
+//
+//  Created by Ehsan Taghavi on 23.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
