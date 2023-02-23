e50802f1e5ec24e3eab5c69375ac30bc646c5525
EhsanTgv <EhsanTgv@gmail.com>
2023-02-23 15:12:22 +0300

    Implement new navigation method

--- NavDemo/ContentView.swift
+++ NavDemo/ContentView.swift
@@ -7,15 +7,51 @@
 
 import SwiftUI
 
+struct Company: Identifiable, Hashable{
+    var id = UUID()
+    let name:String
+}
+struct Stock: Identifiable, Hashable{
+    var id = UUID()
+    let ticker:String
+}
+
 struct ContentView: View {
+    let companies:[Company] = [
+        .init(name: "Apple"),
+        .init(name: "Facebook"),
+        .init(name: "Google"),
+        .init(name: "Microsoft"),
+        .init(name: "Dell"),
+    ]
+    let stocks:[Stock] = [
+        .init(ticker: "APPL"),
+        .init(ticker: "FB"),
+        .init(ticker: "GOOGL"),
+        .init(ticker: "MSFT"),
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            List {
+                Section("Companies"){
+                    ForEach(companies){ company in
+                        NavigationLink(company.name,value: company)
+                    }
+                }
+                Section("Stocks"){
+                    ForEach(stocks) { stock in
+                        NavigationLink(stock.ticker,value: stock)
+                    }
+                }
+            }
+            .navigationDestination(for: Company.self){ company in
+                Text(company.name)
+            }
+            .navigationDestination(for: Stock.self){ stock in
+                Text(stock.ticker)
+                    .font(.headline)
+            }
         }
-        .padding()
     }
 }
 
